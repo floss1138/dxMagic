4f3e63ebd195524026a57e8948c3a95e558be13d
floss1138 <floss1138@gmail.com>
2018-01-03 10:29:04 +0000

    Error check for bad row data added

--- perl/readxlsx1.pl
+++ perl/readxlsx1.pl
@@ -2,15 +2,17 @@
 
 use strict;
 use warnings;
+use Carp;
 use Spreadsheet::Read; # required for xlsx read subroutine but this also requires:
 # for XLSX, Spreadsheet::ParseXLSX needs to be installed - this one is required
 # for XLS, Spreadsheet::ParseExcel needs to be installed
 # for Libre office Spreadsheet::ReadSXC needs to be installed
-# no warnings "uninitialized"; # prevents uninitialized warnings from ParseXLSX for blank cells in xlsx
+
+no warnings "uninitialized"; # prevents uninitialized warnings from ParseXLSX for blank cells in xlsx
 
 our $VERSION = '0.0.01';
 
-my $read_sheet = '/home/user1/dx_xlsx/25-20-3003-AD_from_prodge_all.xlsx';
+my $read_sheet = '/home/user1/dx_xlsx/25-20-3003-AD_from_prodge_all_bad_ending.xlsx';
 
 # call readxlsx sub, xlsx to read is the 1st argument
 readxlsx ($read_sheet);
@@ -25,7 +27,8 @@ my ($xlsxread) = @_;
 print "\nreadxlsx sub has been called, $xlsxread\n";
 
 # If file is not found then the read will simply fail with an uninitialised error so check the file is present:
-if (-e $xlsxread) { print "$xlsxread exists!\n";}
+if (-e $xlsxread) 
+    { print "$xlsxread will be processed:\n";
 local $/ = "\r\n"; # new line defined for windows, might need to be local to read sub?
 my $xlsx = ReadData ("$xlsxread");
 print "Second sheet (attout) cell B3 is:\n";
@@ -39,10 +42,12 @@ $attin_file =~ s/\.xlsx$/.txt/x;
 
 # $attin_file =~ s/$vcdir/$attidir/; # Substitute valid candidate directory with attin directory where file is to be written
 print "\nattin file will be called $attin_file\n";
-open my $FILEOUT, ">", "$attin_file" or die "Cannot open output file $!";  # with append access does not overwrite original.  foreach is OK if file remains open i.e. adds to existing content
+open my $FILEOUT, ">", "$attin_file" or croak "Cannot open output file $!";  # with append access does not overwrite original.  foreach is OK if file remains open i.e. adds to existing content
 local $" = "\t"; # set the output field separator to a tab
+# Processing of spread sheet needs to stop at end of data but if there was a blank row or two then this is ignored 
+# Only if 3 successive blank rows are found is the looping over.  This is achieved using a blank row counter
 my $blankcount=0;
-# Start processing sheet from 3rd line - this is the header.  1st 2 lines left blank as a top margin for notes
+# Start processing sheet from 3rd line - this is the header.  1st 2 lines not used as top margin for notes
 for (my $rowcount=3; $blankcount<3; $rowcount++)
         {
     
@@ -55,22 +60,23 @@ for (my $rowcount=3; $blankcount<3; $rowcount++)
                         }
         if ($row > 1)
                 {
-                # print "row is $row >> @row\n"; This would print out here if there were empty values in a whole line
+                # print "row is $row >> @row\n";
                 my $firstelement = $row[1]; # check second element in array - if its HANDLE or '[any_uppercase_or_numbers x 2] then its a handle value, @row[0] better written $row[0]
                         if ($firstelement =~ /^HANDLE|^\'[A-Z0-9]{2}/)
                         {
-                        # TODO enable if -e to encompase the attin creation
                         print "Row array contains: @row\n"; # send this to a file and you have attout, this prints to screen for debug
-                        # exit 0;
                         # Remove the margin i.e. the 1st element of array 
                         my $margin = shift @row;    
                         print $FILEOUT "@row$/"; # The new line needs to be MS complient hence the $/ defined as \r\n 
                         } # close if ($firstelement...
+                        else { print "!!! Row does not comply with attout format !!! : @row\n"; }
                 } # close if ($row > 1...
         } # close for (my $rowcount...
+   
 print "attin file created\n";
-close($FILEOUT);
+close($FILEOUT) or carp "Could not close $attin_file";
 
+    } # if -e $xlsxread
 } # close sub readxlsd
 
 
